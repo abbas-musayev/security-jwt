fb24cf7c6d2ad112ea1806e57abfd04b473f9af7
Abbas <70229752+abbas-musayev@users.noreply.github.com>
2021-07-09 14:57:18 +0400

    #2

--- src/main/java/az/javatechie/techiesecurity/entity/UserMY.java
+++ src/main/java/az/javatechie/techiesecurity/entity/UserMY.java
@@ -1,11 +1,17 @@
 package az.javatechie.techiesecurity.entity;
 import lombok.*;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
